73ea2c6405ea6bc5fc7acbe94d4b67b5b34efa8f
Jesus Rivera <r.jesus9107@gmail.com>
2023-02-10 14:52:57 -0800

    font styling for "no result" response

--- client/src/pages/search.jsx
+++ client/src/pages/search.jsx
@@ -1,6 +1,7 @@
 import './style.scss'
 import React from 'react'
 import useSearch from '../hooks/useSearch.js'
+import NothingFound from '../assets/nothing-found.svg'
 
 import SearchBox from '../components/SearchBox'
 import WineGrid from '../components/WineGrid'
@@ -15,23 +16,22 @@ const Search = () => {
         <h4>winesearch</h4>
         <div className='cover'></div>
 
-        <div className='landing-content'>
-          <div className='search-container'>
-            <div className='tabs'>
-              <p className={searchBy == 'name' ? "selected" : ""} onClick={() => handleToggle('name')}>By Name</p>
-              <p className={searchBy == 'desc' ? "selected" : ""} onClick={() => handleToggle('desc')}>By Desc:</p>
-            </div>
-            <SearchBox
-              callback={fetchCallback}
-              placeholder={searchBy == "desc" ? "e.g. 'jammy' or 'dry, tannin, cherry'" : "e.g. 'Unruly' 'La Marca' etc."} />
+        <div className='search-container'>
+          <div className='tabs'>
+            <p className={searchBy == 'name' ? "selected" : ""} onClick={() => handleToggle('name')}>By Name</p>
+            <p className={searchBy == 'desc' ? "selected" : ""} onClick={() => handleToggle('desc')}>By Desc:</p>
           </div>
+          <SearchBox
+            callback={fetchCallback}
+            placeholder={searchBy == "desc" ? "e.g. 'jammy' or 'dry, tannin, cherry'" : "e.g. 'Unruly' 'La Marca' etc."} />
         </div>
       </div>
+
       <WineGrid>
         {wine && wine.map((w, i) => w && <Wine key={i} wine={w} />)}
       </WineGrid>
 
-      {nothingFound && <p id="nothing-found">A glass half-full or, perhaps, one half-empty... No, it's definitely empty.</p>}
+      {nothingFound && <p id="nothing-found">Couldn't find that one. But it's been added to the list.<br /> Thanks 👋🏼</p>}
     </>
   )
 }
