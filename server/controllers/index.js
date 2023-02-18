9a982ed1b1b402afb2d9bbdf0d0a0d93da1ca1d8
Jesus Rivera <r.jesus9107@gmail.com>
2023-02-17 20:32:50 -0800

    api updates

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -50,3 +50,46 @@ exports.getWineBy = async (req, res, next) => {
     data: rows
   })
 }
+
+exports.findWine = async (req, res, _) => {
+  let query = "SELECT * FROM wine "
+  const { wineData } = req.body
+
+  if (wineData.type) query += ` WHERE type = '${wineData.type}'`
+  if (wineData.region) query += wineData.type ? ` AND region = '${wineData.region}'` : ` WHERE region = '${wineData.region}'`
+  if (wineData.rating) query += wineData.region || wineData.type ? ` AND rating > '${wineData.rating}'` : ` WHERE rating > '${wineData.rating}';`
+  if (wineData.description && wineData.description[0] != '') {
+    wineData.description.forEach((desc, i) => {
+      if (i > 0) query += ` OR LOCATE('${desc}', description)`
+
+      if (i == 0 && !wineData.description && !wineData.rating && !wineData.type) ` WHERE LOCATE('${desc}', description)`  
+    })
+    query += ';'
+  }
+
+  const [rows] = await pool.query(query)
+  res.status(200).json({
+    status: 'success',
+    length: rows?.length,
+    data: rows
+  })
+}
+
+exports.getTypes = async (_, res) => {
+  const [rows] = await pool.query("SELECT DISTINCT type FROM wine;")
+  res.status(200).json({
+    status: 'success',
+    length: rows?.length,
+    data: rows
+  })
+}
+
+
+
+
+
+
+
+
+
+
--- server/routes/index.js
+++ server/routes/index.js
@@ -4,5 +4,7 @@ const router = express.Router()
 
 router.route('/').get(controllers.getWine).post(controllers.addOneWine)
 router.route('/search-by').post(controllers.getWineBy)
+router.route('/main-search').post(controllers.findWine)
+router.route('/types').get(controllers.getTypes)
 
 module.exports = router
